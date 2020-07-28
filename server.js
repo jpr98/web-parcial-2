d73846efb5709845e02c95c493e09a14356a2ece
Juan Pablo R <jprscs@gmail.com>
2020-07-28 18:17:34 -0500

    Adds comments to server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+// Imports express
 const express = require('express');
 
+// Creates express app instance
 const app = express();
 
+// Set up middlewares
 app.use(express.json());
 
+// Set up routes
 app.get("/", (req, res) => {
     res.send("You are on the homepage");
 });
@@ -22,6 +26,7 @@ app.put("/put/:id", (req, res) => {
     res.send(`Task ${req.params.id} has been updated`);
 });
 
+// Start server on given port
 app.listen(5000, () => {
     console.log("Server listening on port 5000...");
 });
